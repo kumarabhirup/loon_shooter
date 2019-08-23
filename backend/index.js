5f6ca8f9997368af1286cfdc1df6d1b654b4e7c7
Kumar Abhirup <kumar@iqubex.com>
2019-08-23 20:09:06 +0530

    👌 IMPROVE: Use CORS

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({
 
 // CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', process.env.KOJI_FRONTEND_URL) // use '*' as second param to allow any client to hack in
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Jiro-Request-Tag')
   next()
 })
