6e04408c9d3e13d3ef1eb4031876a77787eb9119
Kumar Abhirup <kumar@iqubex.com>
2019-08-25 10:31:32 +0530

    👌 IMPROVE: Declutter the Balloon Grid

--- frontend/app/lib/spawn.js
+++ frontend/app/lib/spawn.js
@@ -1,32 +1,33 @@
 /* eslint-disable no-unused-vars */
-/* global objSize, gameSize, width, height, Balloon, balloons, imgBalloons */
+/* global objSize, gameSize, width, height, Balloon, balloons, imgBalloons, random */
 
 // Spawn Balloons in a grid
 function spawnBalloons() {
   const rows = 5
-  const distance = objSize / 4
-  const rowWidth = gameSize * 0.5
+  const distance = objSize * 0.4 // <- don't change this
+  const rowWidth = gameSize * 0.5 // this is what part of screen balloons will occupy
 
   for (let i = 0; i < rows; i += 1) {
     for (let j = 0; j < rowWidth; j += 1) {
-      let type = 0
-      if (i <= rows - 3) {
-        type = 1
-      }
-      if (i <= rows - 5) {
-        type = 2
-      }
-
       balloons.push(
         new Balloon(
           {
             x: width / 2 + (objSize + distance) * (j - (rowWidth - 1) / 2),
-            y: height * 0.1 + objSize * i,
+            y: height * 0.1 + objSize * i * 1.4,
           },
-          { radius: 0.7 * objSize },
+          {
+            width: 0.7 * objSize,
+            height: 0.7 * objSize,
+            radius: 0.7 * objSize,
+          },
           {
             shape: 'circle',
-            image: imgBalloons[type],
+            // image: imgBalloons[random([0, 1, 2, 3])],
+            color: {
+              r: Math.floor(random(0, 255)),
+              g: Math.floor(random(0, 255)),
+              b: Math.floor(random(0, 255)),
+            },
             shootingBalloon: false,
           }
         )
