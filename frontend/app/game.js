78c051113e2b6245438237d36824c1f2d38cc6b6
Kumar Abhirup <kumar@iqubex.com>
2019-08-27 17:16:09 +0530

    👌 IMPROVE: Lose life when balloon goes out of frame

--- frontend/app/game.js
+++ frontend/app/game.js
@@ -51,6 +51,26 @@ function gamePlay() {
     if (theShootingBalloon.shooting) theShootingBalloon.fire()
 
     if (theShootingBalloon.wentOutOfFrame()) {
+      if (Koji.config.sounds.life) {
+        // eslint-disable-next-line no-loop-func
+        sndLife = loadSound(Koji.config.sounds.life, () =>
+          playMusic(sndLife, 1, false)
+        )
+      }
+
+      floatingTexts.push(
+        new FloatingText(
+          width / 2,
+          height / 2,
+          Koji.config.strings.wentOutOfFrame,
+          Koji.config.colors.floatingTextColor,
+          50
+        )
+      )
+
+      loseLife()
+      score -= 50
+
       // theShootingBalloon.destruct() <- maybe this is not needed
       shootingBalloons.splice(i, 1)
       shooter.reload()
